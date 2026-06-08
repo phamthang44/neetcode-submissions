724cf480b8e5dbeb42cbc617f22296b180f0adf6
Thắng <166624043+phamthang44@users.noreply.github.com>
2026-06-08 08:09:23 +0700

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.java
@@ -0,0 +1,36 @@
+class MyStack {
+
+    int[] data;
+    int size;
+
+    public MyStack() {
+        data = new int[10];
+        size = 0;
+    }
+    
+    public void push(int x) {
+        data[size++] = x;
+
+    }
+    
+    public int pop() {
+        return data[--size];
+    }
+    
+    public int top() {
+        return data[size - 1];
+    }
+    
+    public boolean empty() {
+        return size == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
